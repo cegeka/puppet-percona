cd85adbe2dc1dfb1f641a3a59bf4d80c12a82994
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-05-20 17:20:29 +0200

    Update debug statement

--- manifests/server/package.pp
+++ manifests/server/package.pp
@@ -2,8 +2,7 @@ class percona::server::package($version_shared_compat=undef,$version_shared=unde
 
   $percona_major_version = regsubst($version_server, '^(\d\.\d)\.(\d+)-(.*)','\1')
   $_percona_major_version = regsubst($percona_major_version, '\.', '', 'G')
-  debug("percona_major_version = ${percona_major_version}")
-  debug("_percona_major_version = ${_percona_major_version}")
+  debug("Percona major version = ${percona_major_version}")
 
   package {
     'Percona-Server-shared-compat' :
