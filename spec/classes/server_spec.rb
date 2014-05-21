145ab4c7db95a2f99d6ec4a555b643c5dd82f0d4
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-05-21 09:26:35 +0200

    Fix server spec

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -32,16 +32,24 @@ describe 'percona::server' do
         Puppet::Error, /parameter version_client must be provided/
       )}
     end
+
+    context 'without version_debuginfo' do
+      let (:params) { { :version_shared_compat => '5.5.31-rel30.3.520.rhel6', :version_shared => '5.5.31-rel30.3.520.rhel6', :version_server => '5.5.31-rel30.3.520.rhel6', :version_client => '5.5.31-rel30.3.520.rhel6' } }
+      it { expect { subject }.to raise_error(
+        Puppet::Error, /parameter version_debuginfo must be provided/
+      )}
+    end
   end
 
   context 'with parameters' do
-    context 'version_shared_compat => 5.5.31-rel30.3.520.rhel6, version_shared => 5.5.31-rel30.3.520.rhel6, version_server => 5.5.31-rel30.3.520.rhel6, version_client => 5.5.31-rel30.3.520.rhel6' do
-      let (:params) { { :version_shared_compat => '5.5.31-rel30.3.520.rhel6', :version_shared => '5.5.31-rel30.3.520.rhel6', :version_server => '5.5.31-rel30.3.520.rhel6', :version_client => '5.5.31-rel30.3.520.rhel6' } }
+    context 'version_shared_compat => 5.5.31-rel30.3.520.rhel6, version_shared => 5.5.31-rel30.3.520.rhel6, version_server => 5.5.31-rel30.3.520.rhel6, version_client => 5.5.31-rel30.3.520.rhel6, version_debuginfo => 5.5.31-rel30.3.520.rhel6' do
+      let (:params) { { :version_shared_compat => '5.5.31-rel30.3.520.rhel6', :version_shared => '5.5.31-rel30.3.520.rhel6', :version_server => '5.5.31-rel30.3.520.rhel6', :version_client => '5.5.31-rel30.3.520.rhel6', :version_debuginfo => '5.5.31-rel30.3.520.rhel6' } }
 
       it { should contain_class('percona::server::package').with_version_shared_compat('5.5.31-rel30.3.520.rhel6')}
       it { should contain_class('percona::server::package').with_version_shared('5.5.31-rel30.3.520.rhel6')}
       it { should contain_class('percona::server::package').with_version_server('5.5.31-rel30.3.520.rhel6')}
       it { should contain_class('percona::server::package').with_version_client('5.5.31-rel30.3.520.rhel6')}
+      it { should contain_class('percona::server::package').with_version_debuginfo('5.5.31-rel30.3.520.rhel6')}
 
       it { should contain_class('percona::server::config').with_data_dir('/data/mysql') }
       it { should contain_class('percona::server::config').with_tmp_dir('/data/mysql_tmp') }
