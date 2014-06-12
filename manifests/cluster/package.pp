0c9c5c294cc11aaf6692a1da752cff129822ca45
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-06-12 10:54:24 +0200

    Added debug package for installing cluster

--- manifests/cluster/package.pp
+++ manifests/cluster/package.pp
@@ -1,30 +1,58 @@
-class percona::cluster::package($version_shared_compat=undef, $version_server=undef, $version_client=undef, $version_galera=undef, $versionlock=false) {
+class percona::cluster::package(
+  $version_shared_compat=undef,
+  $version_server=undef,
+  $version_client=undef,
+  $version_debuginfo=undef,
+  $version_galera=undef,
+  $version_galera_debuginfo=undef,
+  $versionlock=false) {
+
+  $percona_major_version = regsubst($version_server, '^(\d\.\d)\.(\d+)-(.*)','\1')
+  $_percona_major_version = regsubst($percona_major_version, '\.', '', 'G')
+  debug("Percona major version = ${percona_major_version}")
+
+  $galera_major_version = regsubst($version_galera, '^(\d)\.(\d)-(.*)','\1')
+  $_galera_major_version = regsubst($galera_major_version, '\.', '', 'G')
+  debug("Galera major version = ${galera_major_version}")
 
   package {
     'Percona-Server-shared-compat' :
       ensure => $version_shared_compat;
-    'Percona-XtraDB-Cluster-server' :
+    "Percona-XtraDB-Cluster-server-${_percona_major_version}" :
       ensure => $version_server;
-    'Percona-XtraDB-Cluster-client' :
+    "Percona-XtraDB-Cluster-client-${_percona_major_version}" :
       ensure => $version_client;
-    'Percona-XtraDB-Cluster-galera' :
+    "Percona-XtraDB-Cluster-${_percona_major_version}-debuginfo" :
+      ensure => $version_debuginfo;
+    "Percona-XtraDB-Cluster-galera-${_galera_major_version}" :
       ensure => $version_galera;
+    "Percona-XtraDB-Cluster-galera-${_galera_major_version}-debuginfo" :
+      ensure => $version_galera_debuginfo;
   }
 
-  Package['Percona-Server-shared-compat'] -> Package['Percona-XtraDB-Cluster-server'] -> Package['Percona-XtraDB-Cluster-client'] -> Package['Percona-XtraDB-Cluster-galera']
+  Package['Percona-Server-shared-compat']
+    -> Package["Percona-XtraDB-Cluster-galera-${_galera_major_version}"]
+    -> Package["Percona-XtraDB-Cluster-galera-${_galera_major_version}-debuginfo"]
+    -> Package["Percona-XtraDB-Cluster-server-${_percona_major_version}"]
+    -> Package["Percona-XtraDB-Cluster-client-${_percona_major_version}"]
+    -> Package["Percona-XtraDB-Cluster-${_percona_major_version}-debuginfo"]
 
   case $versionlock {
     true: {
       packagelock { 'Percona-Server-shared-compat': }
-      packagelock { 'Percona-XtraDB-Cluster-server': }
-      packagelock { 'Percona-XtraDB-Cluster-client': }
-      packagelock { 'Percona-XtraDB-Cluster-galera': }
+      packagelock { "Percona-XtraDB-Cluster-server-${_percona_major_version}": }
+      packagelock { "Percona-XtraDB-Cluster-client-${_percona_major_version}": }
+      packagelock { "Percona-XtraDB-Cluster-${_percona_major_version}-debuginfo": }
+      packagelock { "Percona-XtraDB-Cluster-galera-${_galera_major_version}": }
+      packagelock { "Percona-XtraDB-Cluster-galera-${_galera_major_version}-debuginfo": }
     }
     false: {
       packagelock { 'Percona-Server-shared-compat': ensure => absent }
-      packagelock { 'Percona-XtraDB-Cluster-server': ensure => absent }
-      packagelock { 'Percona-XtraDB-Cluster-client': ensure => absent }
-      packagelock { 'Percona-XtraDB-Cluster-galera': ensure => absent }
+      packagelock { "Percona-XtraDB-Cluster-server-${_percona_major_version}": ensure => absent }
+      packagelock { "Percona-XtraDB-Cluster-client-${_percona_major_version}": ensure => absent }
+      packagelock { "Percona-XtraDB-Cluster-${_percona_major_version}-debuginfo": ensure => absent }
+      packagelock { "Percona-XtraDB-Cluster-galera-${_galera_major_version}": ensure => absent }
+      packagelock { "Percona-XtraDB-Cluster-galera-${_galera_major_version}-debuginfo": ensure => absent }
     }
     default: { fail('Class[Percona::Cluster::Package]: parameter versionlock must be true or false')}
   }
