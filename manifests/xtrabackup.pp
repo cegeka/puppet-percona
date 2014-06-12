401752e92d8e04fcb024e87ac68e72d31aa2f3b2
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-06-12 10:54:24 +0200

    Added debug package for installing cluster

--- manifests/xtrabackup.pp
+++ manifests/xtrabackup.pp
@@ -1,19 +1,31 @@
-class percona::xtrabackup($version=undef, $versionlock=false) {
+class percona::xtrabackup(
+  $version=undef,
+  $version_debuginfo=undef,
+  $versionlock=false
+) {
 
   if ! $version {
     fail('Class[Percona::Xtrabackup]: parameter version must be provided')
   }
+  if ! $version_debuginfo {
+    fail('Class[Percona::Xtrabackup]: parameter version_debuginfo must be provided')
+  }
 
   package { 'percona-xtrabackup':
+    ensure => $version
+  }
+  package { 'percona-xtrabackup-debuginfo':
     ensure => $version
   }
 
   case $versionlock {
     true: {
       packagelock { 'percona-xtrabackup': }
+      packagelock { 'percona-xtrabackup-debuginfo': }
     }
     false: {
       packagelock { 'percona-xtrabackup': ensure => absent }
+      packagelock { 'percona-xtrabackup-debuginfo': ensure => absent }
     }
     default: { fail('Class[Percona::Xtrabackup]: parameter versionlock must be true or false')}
   }
