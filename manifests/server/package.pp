d59275bb74f3d84be649d044badaaed1a8a520ea
Fabian Dammekens <fabian.dammekens@cegeka.be>
2016-11-03 16:11:52 +0100

    [PERCONA] Add missing parameters for cluster vs server

--- manifests/server/package.pp
+++ manifests/server/package.pp
@@ -1,4 +1,5 @@
 class percona::server::package(
+  $package_shared_compat=undef
   $version_shared_compat=undef,
   $version_shared=undef,
   $version_server=undef,
@@ -11,8 +12,14 @@ class percona::server::package(
   $_percona_major_version = regsubst($percona_major_version, '\.', '', 'G')
   debug("Percona major version = ${percona_major_version}")
 
+  if defined($package_shared_compat) {
+    $real_package_shared_compat = $package_shared_compat
+  } else {
+    $real_package_shared_compat = 'Percona-Server-shared-compat'
+  }
+
   package {
-    'Percona-Server-shared-compat' :
+    $real_package_shared_compat :
       ensure => $version_shared_compat;
     "Percona-Server-shared-${_percona_major_version}" :
       ensure => $version_shared;
@@ -24,7 +31,7 @@ class percona::server::package(
       ensure => $version_debuginfo;
   }
 
-  Package['Percona-Server-shared-compat']
+  Package[$real_package_shared_compat]
     -> Package["Percona-Server-shared-${_percona_major_version}"]
     -> Package["Percona-Server-server-${_percona_major_version}"]
     -> Package["Percona-Server-client-${_percona_major_version}"]
@@ -32,14 +39,14 @@ class percona::server::package(
 
   case $versionlock {
     true: {
-      packagelock { 'Percona-Server-shared-compat': }
+      packagelock { $real_package_shared_compat : }
       packagelock { "Percona-Server-shared-${_percona_major_version}": }
       packagelock { "Percona-Server-server-${_percona_major_version}": }
       packagelock { "Percona-Server-client-${_percona_major_version}": }
       packagelock { "Percona-Server-${_percona_major_version}-debuginfo": }
     }
     false: {
-      packagelock { 'Percona-Server-shared-compat': ensure => absent }
+      packagelock { $real_package_shared_compat : ensure => absent }
       packagelock { "Percona-Server-shared-${_percona_major_version}": ensure => absent }
       packagelock { "Percona-Server-server-${_percona_major_version}": ensure => absent }
       packagelock { "Percona-Server-client-${_percona_major_version}": ensure => absent }
