d0c796cb60d0dc5c8716e7b0f21e0658b31393d8
andreias <andrei.asan@cegeka.com>
2017-04-21 13:32:05 +0200

    [PERCONA] change service to class

--- manifests/provision/database.pp
+++ manifests/provision/database.pp
@@ -1,25 +1,14 @@
 define percona::provision::database($ensure) {
 
-  $myservice = $::operatingsystem ? {
-    /RedHat|Fedora|CentOS/  => 'mysqld',
-    default                 => 'mysql',
-  }
+  include percona::provision::service
 
   if $::mysql_exists {
     mysql_database { $name:
       ensure  => $ensure,
-      require => [Service[$myservice], File['/root/.my.cnf'] ]
+      require => [Service["${::percona::provision::service::myservice}"], File['/root/.my.cnf'] ]
     }
   } else {
     fail("Mysql binary not found, Fact[::mysql_exists]:${::mysql_exists}")
-  }
-
-  service { $myservice:
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => true,
-    require    => [ Class[Percona::Server::Package], File['/root/.my.cnf'] ],
   }
 
 }
