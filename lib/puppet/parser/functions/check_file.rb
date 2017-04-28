62fe07b010b07119b3184a67810b4bb9df90832d
andreias <andrei.asan@cegeka.com>
2017-04-28 16:03:29 +0200

    [PERCONA] fix .my.cnf replacement

--- lib/puppet/parser/functions/check_file.rb
+++ lib/puppet/parser/functions/check_file.rb
@@ -1,11 +1,15 @@
 
 module Puppet::Parser::Functions
-	newfunction(:check_file, :type => :rvalue) do |args|
-    if File.read(args[0]) =~ /args[1]/ then
-      return 0
+  newfunction(:check_file, :type => :rvalue) do |args|
+    if File.file?(args[0]) then 
+      if File.read(args[0]) =~ /args[1]/ then
+        return 0
+      else
+        return 1
+      end
     else
       return 1
     end
-	end
+  end
 end
 
