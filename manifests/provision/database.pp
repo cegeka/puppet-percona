45b17727c8f14026e3460cd5f44803ad1ea22e74
andreias <andrei.asan@cegeka.com>
2017-06-08 10:34:27 +0200

    [PERCONA] user rights for cluster

--- manifests/provision/database.pp
+++ manifests/provision/database.pp
@@ -1,11 +1,18 @@
-define percona::provision::database($ensure) {
+define percona::provision::database($ensure, $type='server') {
 
-  include percona::provision::service
-
+  if $type == 'server' {
+    include percona::provision::service
+    $real_service = "${::percona::provision::service::myservice}"
+  }
+  else
+  {
+    include percona::provision::service_cluster
+    $real_service = "${::percona::provision::service_cluster::myservice}"
+  }
   if $::mysql_exists {
     mysql_database { $name:
       ensure  => $ensure,
-      require => Service["${::percona::provision::service::myservice}"]
+      require => Service["${service}"]
     }
   } else {
     fail("Mysql binary not found, Fact[::mysql_exists]:${::mysql_exists}")
--- manifests/provision/rights.pp
+++ manifests/provision/rights.pp
@@ -27,12 +27,20 @@ define percona::provision::rights(
   $secret_id=undef,
   $host='localhost',
   $ensure='present',
-  $priv='all'
+  $priv='all',
+  $type='server'
 ) {
 
-  include percona::provision::service
-
   if $::mysql_exists {
+  if $type == 'server' {
+    include percona::provision::service
+    $real_service = "${::percona::provision::service::myservice}"
+  }
+  else
+  {
+    include percona::provision::service_cluster
+    $real_service = "${::percona::provision::service_cluster::myservice}"
+  }
     if $secret_id == undef and $password == undef {
       fail('You must privide a password or a secret_id to ::mysql::rights')
     }
@@ -47,14 +55,14 @@ define percona::provision::rights(
       ensure        => $ensure,
       password_hash => mysql_password($mysql_password),
       provider      => 'mysql',
-      require       => Service["${::percona::provision::service::myservice}"]
+      require       => Service["${real_service}"]
     })
 
     if $ensure == 'present' {
       mysql_grant { "${user}@${host}/${database}":
         privileges => $priv,
         provider   => 'mysql',
-        require    => [ Percona_user["${user}@${host}"], Service["${::percona::provision::service::myservice}"] ]
+        require    => [ Percona_user["${user}@${host}"], Service["${real_service}"] ]
       }
     }
 
