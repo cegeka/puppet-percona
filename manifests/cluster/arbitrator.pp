a1e1e44e261748ce5dcf06d69973a2b11ce3b665
Paul Herbosch <paul.herbosch@cegeka.be>
2019-08-05 13:21:38 +0200

    [PERCONA] support for latest Percona 5.7, new rpm key, arbitrator service

---
+++ manifests/cluster/arbitrator.pp
@@ -0,0 +1,38 @@
+# Class: percona::cluster::arbitrator
+#
+# Usage: use this to add a node to an exiting percona cluster
+#
+class percona::cluster::arbitrator (
+  $garb_version     = undef,
+  $garb_packagename = undef,
+  $versionlock      = undef,
+  $cluster_address  = undef,
+  $cluster_name     = undef,
+) {
+
+  package { $garb_packagename:
+    ensure => $garb_version
+  }
+
+  file { '/etc/sysconfig/garb':
+    ensure  => 'file',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('profile/iac/database/garb.sysconfig.erb'),
+    require => Package[$garb_packagename]
+  }
+
+  service { 'garb':
+    ensure  => 'running',
+    enable  => true,
+    require => [File['/etc/sysconfig/garb'],Package[$garb_packagename]]
+  }
+
+  if $versionlock {
+    packagelock { "${garb_packagename}-${garb_version}": }
+  } else {
+    packagelock { "${garb_packagename}-${garb_version}": ensure => absent }
+  }
+
+}
