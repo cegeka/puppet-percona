b7a383f5e842e1c0b7a1de11629ea29ea6e21b32
Fabian Dammekens <fabian@dammekens.be>
2022-04-26 18:14:48 +0200

    [PERCONA] Code to enable plugins and its config -- Example: audit_log

--- manifests/provision/plugin.pp
+++ manifests/provision/plugin.pp
@@ -1,6 +1,18 @@
-define percona::provision::plugin($ensure, $type='server') {
+define percona::provision::plugin(
+  $ensure,
+  $type='server',
+  $config_dir='/etc/my.cnf.d',
+  $config=undef
+) {
 
   if $::mysql_exists {
+    if ($config) {
+      file { "${config_dir}/${name}.cnf" :
+        ensure => present,
+        content => template("${module_name}/plugin.cnf.erb"),
+        require => Mysql_plugin[$name]
+      }
+    }
     mysql_plugin { $name:
       ensure  => $ensure,
       require => Service['mysqld']
