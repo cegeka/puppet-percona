dc465e5a73312023064896ec9fc4a24d12924e0f
Bogdan Stan <bogdan.stan@cegeka.com>
2024-02-22 15:52:48 +0100

    fixed the mysql function

--- lib/puppet/parser/functions/mysql_password.rb
+++ lib/puppet/parser/functions/mysql_password.rb
@@ -1,9 +1,9 @@
 # hash a string as mysql's "PASSWORD()" function would do it
-require 'digest/sha1'
+require 'digest/sha2'
 
 module Puppet::Parser::Functions
 	newfunction(:mysql_password, :type => :rvalue) do |args|
-		'*' + Digest::SHA1.hexdigest(Digest::SHA1.digest(args[0])).upcase
+		'*' + Digest::SHA2.hexdigest(Digest::SHA2.digest(args[0])).upcase
 	end
 end
 
