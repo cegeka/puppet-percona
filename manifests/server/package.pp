8cdfd9659abc6a4875c00f4e1b3697c58835dcac
Warre-Tielens <warretielens@gmail.com>
2025-08-26 08:29:40 +0200

    Add logic for both 57 and 80

--- manifests/server/package.pp
+++ manifests/server/package.pp
@@ -14,32 +14,40 @@ class percona::server::package (
   $percona_package_version = regsubst($version_server, '^(.*?)-(.*)', '\1')
   $percona_package_release = regsubst($version_server, '^(.*?)-(.*)', '\2')
 
-  if $package_name {
-    $real_package_name = $package_name
-  } else {
-    $real_package_name = "Percona-Server-server-${_percona_major_version}"
+  $default_server_pkg = $_percona_major_version ? {
+   '57' => "Percona-Server-server-57",
+   '80' => 'percona-server-server',
   }
 
-  # Base package list (always installed)
-  $base_packages = {
-    $real_package_name       => $version_server,
-    $xtrabackup_name         => $version_xtrabackup,
-    'percona-server-client'  => $version_server,
-    'percona-server-shared'  => $version_server,
-    'percona-icu-data-files' => $version_server,
+  $real_package_name = $package_name ? {
+    undef   => $default_server_pkg,
+    default => $package_name,
   }
 
-  # OS-Specific package
-  $extra_packages = $facts['os']['release']['major'] ? {
-    '8'  => { 'percona-server-shared-compat' => $version_server },
+  # 5.7 uses "Percona-Server-<..>-57" (Capitalized); 8.0 uses "percona-server-<..>" (lowercase).
+  $packages_by_major = {
+    '57' => {
+      $real_package_name                => $version_server,
+      $xtrabackup_name                  => $version_xtrabackup,
+      "Percona-Server-client-57"        => $version_server,
+      "Percona-Server-shared-57"        => $version_server,
+      "Percona-Server-shared-compat-57" => $version_server,
+    },
+    '80' => {
+      $real_package_name                => $version_server,
+      $xtrabackup_name                  => $version_xtrabackup,
+      "percona-server-client"           => $version_server,
+      "percona-server-shared"           => $version_server,
+      "percona-icu-data-files"          => $version_server,
+    },
   }
 
-  # Merge base and OS-specific package
-  $packages = merge($base_packages, $extra_packages)
+  $packages_to_install = $packages_by_major[$_percona_major_version]
 
-  $packages.each |String $pkg_name, String $pkg_version| {
+  # create package resources
+  $packages_to_install.each |$pkg_name, $pkg_ensure| {
     package { $pkg_name:
-      ensure => $pkg_version,
+      ensure => $pkg_ensure,
     }
   }
 
@@ -49,23 +57,16 @@ class percona::server::package (
     $versionlock_ensure = absent
   }
 
-  # Base versionlock components
-  $versionlock_components = ['server-server', 'server-shared', 'server-client', 'icu-data-files']
-
-  # OS-Specific versionlock components
-  $extra_versionlock_components = $facts['os']['release']['major'] ? {
-    '8'  => ['server-shared-compat'],
-  }
-
-  $all_versionlock_components = concat($versionlock_components, $extra_versionlock_components)
-
-  $all_versionlock_components.each |String $component| {
-    yum::versionlock { "percona-${component}":
-      ensure  => $versionlock_ensure,
-      version => $percona_package_version,
-      release => $percona_package_release,
-      epoch   => 0,
-      arch    => 'x86_64',
+  # create/remove versionlock entries for packages
+  $packages_to_install.each |$pkg_name, $pkg_ensure| {
+    if $pkg_ensure =~ /-/ {
+      yum::versionlock { $pkg_name:
+        ensure  => $versionlock_ensure,
+        version => $percona_package_version,
+        release => $percona_package_release,
+        arch    => 'x86_64',
+        epoch   => 0,
+      }
     }
   }
 }
