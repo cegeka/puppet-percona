04c671961e5e21360a45ced72fd9f429b7f5e47b
Christophe Calus <christophe.calus@cegeka.com>
2025-12-11 20:41:18 +0100

    fix: Correcting the parameters

--- manifests/provision/binlog_admin_user.pp
+++ manifests/provision/binlog_admin_user.pp
@@ -3,16 +3,16 @@
 # @param user
 #   The MySQL user to grant or revoke BINLOG_ADMIN privilege
 # @param host
-#   The host from which the user connects (default: 'localhost')
+#   The host from which the user connects (default: '%')
 # @param ensure
-#   Whether to grant (true) or revoke (false) the privilege (default: false)
+#   Whether to grant (present) or revoke (absent) the privilege (default: present)
 #
 define percona::provision::binlog_admin_user (
   String $user,
-  String $host = 'localhost',
-  Boolean $ensure = false,
+  String $host = '%',
+  Enum['present', 'absent'] $ensure = 'present',
 ) {
-  if $ensure {
+  if $ensure == 'present' {
     exec { "grant_binlog_admin_${user}_${host}":
       command => "mysql --defaults-file=/root/.my.cnf -e \"GRANT BINLOG_ADMIN ON *.* TO '${user}'@'${host}' WITH GRANT OPTION\"",
       unless  => "mysql --defaults-file=/root/.my.cnf -e \"SHOW GRANTS FOR '${user}'@'${host}'\" 2>/dev/null | grep -iq BINLOG_ADMIN",
